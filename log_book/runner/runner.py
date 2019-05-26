aa9630d2c71f5ff1936654cd00ea4cf7e3a2bd21
mahsa <mahsa.lotfi92@gmail.com>
2019-05-26 18:58:15 +0200

    handling file reading error

--- log_book/runner/runner.py
+++ log_book/runner/runner.py
@@ -1,6 +1,7 @@
 import json
 import sys
 from datetime import datetime
+from json.decoder import JSONDecodeError
 
 from log_book.runner.processor import process_way_points
 
@@ -17,13 +18,13 @@ def runner():
         print('Please provide a file name to read the waypoint data')
         return
 
-    if sys.argv[1].split('.')[-1] != 'json':
-        print('Input file should be json')
-        return
-
     with open(sys.argv[1]) as json_file:
-        waypoints = json.load(json_file)
-        trips = process_way_points(waypoints)
-        f = open("trips.json", 'w')
-        f.write(json.dumps(trips, cls=Encoder))
-        f.close()
+        try:
+            way_points = json.loads(json_file.read())
+            trips = process_way_points(way_points)
+            with open("trips.json", 'w') as trips_file:
+                trips_file.write(json.dumps(trips, cls=Encoder))
+                trips_file.close()
+        except JSONDecodeError:
+            print('Input file should be json file.')
+        json_file.close()
